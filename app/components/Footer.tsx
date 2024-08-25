49e863d25f4aede2dd47fdfb5573d80854e212a7
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-25 11:40:02 +0900

    フッターを追加

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border mt-8">
+      <div className="container mx-auto px-4 py-6 text-center">
+        <p className="text-gray-600">&copy; 2024 Taiyo Suzuki. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
