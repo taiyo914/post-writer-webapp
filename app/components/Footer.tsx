7c3efbeb4f77ed20157f0978143f5f0426ab4127
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-25 13:02:23 +0900

    レイアウトを修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-white border ">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p className="text-gray-600">&copy; 2024 Taiyo Suzuki. All Rights Reserved.</p>
+        <p className="text-gray-600 text-sm ">&copy; 2024 Taiyo Suzuki. All Rights Reserved.</p>
       </div>
     </footer>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
-        <main className="min-h-screen">
-          {children}
-        </main>
-        <Footer/>
+        <div className="flex flex-col min-h-screen">
+          <Header/>
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer/>
+        </div>
       </body>
     </html>
   );
