d338137d2f62470b802be7a37a09b81386e38d7d
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-25 11:40:13 +0900

    コンテナを修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-
 const Header = () => {
   return (
     <header className="bg-white shadow-md">
@@ -9,10 +8,18 @@ const Header = () => {
           <Link href="/">Vocab Deck</Link>
         </h1>
         <nav className="space-x-4">
-          <a href="#" className="text-gray-600 hover:text-gray-900">Home</a>
-          <a href="#" className="text-gray-600 hover:text-gray-900">About</a>
-          <a href="#" className="text-gray-600 hover:text-gray-900">Services</a>
-          <a href="#" className="text-gray-600 hover:text-gray-900">Contact</a>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            Home
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            About
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            Services
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            Contact
+          </a>
         </nav>
       </div>
     </header>
--- app/components/VocabList.tsx
+++ app/components/VocabList.tsx
@@ -1,49 +1,51 @@
-import React from 'react'
-import { wordsData } from '../data/wordsData'
+import React from "react";
+import { wordsData } from "../data/wordsData";
 
 const VocabList = () => {
   return (
-    <div className="p-4  min-w-[1200px]">
-      <div className="bg-white rounded-lg p-6 border shadow-md">
-      <div className="flex items-center mb-2">
-          <div className="w-10 mr-4"></div> {/* 優先度の位置合わせ用 */}
-          <div className="flex-1 grid grid-cols-8 px-4">
-            <div className="col-span-1 font-bold text-center pr-3">語句 </div>
-            <div className="col-span-1 font-bold text-center px-3">意味 </div>
-            <div className="col-span-2 font-bold text-center px-3">例文 </div>
-            <div className="col-span-2 font-bold text-center px-3">例文訳 </div>
-            <div className="col-span-2 font-bold text-center px-3">メモ </div>
-          </div>
-        </div>
-        <div className="space-y-3">
-          {wordsData.map((word, index) => (
-            <div key={index} className="flex items-center ">
-              <div className="flex justify-center items-center h-10 w-10 bg-gray-300 rounded-full text-lg font-bold mr-4">
-                {word.priority}
-              </div>
-              <div className="flex-1 grid grid-cols-8 border-gray-200 bg-white border shadow rounded-lg px-4 py-3">
-                <div className="col-span-1 flex items-center  border-r border-gray-200 pr-3 font-bold">
-                  {word.word}
-                </div>
-                <div className="col-span-1 flex items-center justify-center border-r border-gray-200 px-3">
-                  {word.meaning}
-                </div>
-                <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
-                  {word.example}
-                </div>
-                <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
-                  {word.exampleTranslation}
-                </div>
-                <div className="col-span-2 flex items-center pl-3 text-sm">
-                  {word.note}
-                </div>
-              </div>
+    <div className="p-4 ">
+      <div className="bg-white rounded-lg p-6 border shadow-md overflow-x-auto">
+        <div className="min-w-[1200px]">
+          <div className="flex items-center mb-2">
+            <div className="w-10 mr-4"></div> {/* 優先度の位置合わせ用 */}
+            <div className="flex-1 grid grid-cols-8 px-4">
+              <div className="col-span-1 font-bold text-center pr-3">語句 </div>
+              <div className="col-span-1 font-bold text-center px-3">意味 </div>
+              <div className="col-span-2 font-bold text-center px-3">例文 </div>
+              <div className="col-span-2 font-bold text-center px-3">例文訳 </div>
+              <div className="col-span-2 font-bold text-center px-3">メモ </div>
             </div>
-          ))}
+          </div>
+          <div className="space-y-3">
+            {wordsData.map((word, index) => (
+              <div key={index} className="flex items-center ">
+                <div className="flex justify-center items-center h-10 w-10 bg-gray-300 rounded-full text-lg font-bold mr-4">
+                  {word.priority}
+                </div>
+                <div className="flex-1 grid grid-cols-8 border-gray-200 bg-white border shadow rounded-lg px-4 py-3">
+                  <div className="col-span-1 flex items-center  border-r border-gray-200 pr-3 font-bold">
+                    {word.word}
+                  </div>
+                  <div className="col-span-1 flex items-center justify-center border-r border-gray-200 px-3">
+                    {word.meaning}
+                  </div>
+                  <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
+                    {word.example}
+                  </div>
+                  <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
+                    {word.exampleTranslation}
+                  </div>
+                  <div className="col-span-2 flex items-center pl-3 text-sm">
+                    {word.note}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VocabList
+export default VocabList;
