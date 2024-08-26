ccb2f167970fd48b872e9be9b58832046abe1a02
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-26 15:06:27 +0900

    復習画面を修正。かなりいい感じ！

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,66 +1,94 @@
 "use client";
-import { useState, ChangeEvent } from "react";
+
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+import { wordsData } from "../data/wordsData";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { wordsData } from "../data/wordsData";
+import { Swiper as SwiperType } from 'swiper';
 
-const CardSlider = () => {
-  const [slider, setSlider] = useState(0);
-  const cards = [
-    "word",
-    "meaning",
-    "example",
-    "translation",
-    "memo",
-    "next word",
-    "Break the ice",
-    "緊張をほぐす",
-    "At the beginning of the meeting, she tried to break the ice by telling a joke.",
-    "会議の冒頭で、彼女はジョークを言って緊張をほぐそうとしました。",
-    "初対面の人との会話や、緊張感のある場面で使えるフレーズ。",
-  ];
+const SwiperComponent = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [priorityValues, setPriorityValues] = useState(
+    wordsData.map((item) => item.priority)
+  );
 
-  const handleSliderChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSlider(Number(e.target.value));
+  const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPriority = parseInt(e.target.value, 10);
+    setPriorityValues((prev) => {
+      const newValues = [...prev];
+      const cardIndex = Math.floor(activeIndex / 5); // 各cardの基準に基づくindexを取得
+      newValues[cardIndex] = newPriority;
+      return newValues;
+    });
+  };
+
+  const handleSlideChange = (swiper: SwiperType) => {
+    setActiveIndex(swiper.activeIndex);
   };
 
   return (
-    <>
-      <div className="h-screen p-4 flex flex-col items-center justify-center">
-        <Swiper
-          modules={[Navigation, Pagination]}
-          navigation
-          pagination={{ type: "progressbar" }}
-          className="w-full max-w-3xl h-4/5 border p-2 rounded-lg"
-        >
-          {cards.map((card, i) => (
-            <>
-              <SwiperSlide key={i}>
-                <div className="w-full h-full bg-white shadow-lg flex items-center justify-center text-2xl flex-col px-20 pt-3">
-                  <div className="mb-4">{card}</div>
-                  <div className="flex w-full items-center justify-center absolute bottom-1 left-1/2 transform -translate-x-1/2">
-                    <input
-                      type="range"
-                      name="rating"
-                      min="0"
-                      max="10"
-                      value={slider}
-                      onChange={handleSliderChange}
-                      className="w-3/5  "
-                    />
-                    <div className="px-2">{slider}</div>
-                  </div>
-                </div>
-              </SwiperSlide>
-            </>
-          ))}
-        </Swiper>
+    <div className="h-screen p-4 flex flex-col items-center ">
+      <Swiper
+        navigation
+        pagination={{ clickable: true ,  type: "progressbar" }}
+        onSlideChange={handleSlideChange}
+        modules={[Navigation, Pagination]}
+        className="w-full max-w-3xl h-3/5 border p-2 rounded-lg"
+      >
+        {wordsData.map((card, i) => (
+          <React.Fragment key={i}>
+            <SwiperSlide>
+              <div className="text-gray-400 pt-3 pl-3 ">Word</div>
+              <div className="h-full flex items-center justify-center text-3xl -mt-8 px-20">
+                <div className="font-bold">{card.word}</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-gray-400 pt-3 pl-3 ">Meaning</div>
+              <div className="w-full h-full  flex items-center justify-center text-3xl -mt-8 px-20">
+                <div className="font-bold">{card.meaning}</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide> 
+            <div className="text-gray-400 pt-3 pl-3 ">Example</div>
+              <div className="w-full h-full flex items-center justify-center text-2xl -mt-8 px-20">
+                <div className="leading-normal">{card.example}</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-gray-400 pt-3 pl-3 ">Translation</div>
+              <div className="w-full h-full flex items-center justify-center text-2xl -mt-8 px-20">
+                <div className="leading-normal">{card.exampleTranslation}</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-gray-400 pt-3 pl-3 ">Memo</div>
+              <div className="w-full h-full flex items-center justify-center text-xl -mt-8 px-20">
+                <div className="leading-normal">{card.note}</div>
+              </div>
+            </SwiperSlide>
+          </React.Fragment>
+        ))}
+      </Swiper>
+
+      <div className="mt-4 w-full text-center flex justify-center">
+        <input
+          type="range"
+          name="priority"
+          min="0"
+          max="10"
+          value={priorityValues[Math.floor(activeIndex / 5)]}
+          onChange={handleSliderChange}
+          className="w-2/3"
+        />
+        <div className="px-2">{priorityValues[Math.floor(activeIndex / 5)]}</div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default CardSlider;
+export default SwiperComponent;
