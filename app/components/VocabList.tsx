d902222912f260f212b04ab37e33cd7ab5ecf475
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-28 13:01:59 +0900

    ページネーション機能を追加

--- app/components/VocabList.tsx
+++ app/components/VocabList.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { wordsData } from "../data/wordsData";
 
 type Settings = {
@@ -14,35 +15,47 @@ interface VocabListProps {
 }
 
 const VocabList: React.FC<VocabListProps> = ({ settings }) => {
-  
   const { sortOrder, displayCount, priorityRange, dateRange } = settings;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
 
   let filteredWords = wordsData
-  //優先度でフィルター
-  .filter(word => 
-    word.priority >= priorityRange[0] && word.priority <= priorityRange[1]
-  )
-  //日付でフィルター
-  .filter(word => {
-    const wordDate = new Date(word.date); 
-    const startDate = dateRange[0] ? new Date(dateRange[0]) : null;
-    const endDate = dateRange[1] ? new Date(dateRange[1]) : null;
+    //優先度でフィルター
+    .filter(
+      (word) =>
+        word.priority >= priorityRange[0] && word.priority <= priorityRange[1]
+    )
+    //日付でフィルター
+    .filter((word) => {
+      const wordDate = new Date(word.date);
+      const startDate = dateRange[0] ? new Date(dateRange[0]) : null;
+      const endDate = dateRange[1] ? new Date(dateRange[1]) : null;
 
-    if (startDate && endDate) {
-      return wordDate >= startDate && wordDate <= endDate;
-    } else if (startDate) {
-      return wordDate >= startDate;
-    } else if (endDate) {
-      return wordDate <= endDate;
-    } else {
-      return true;
-    }
-  });
+      if (startDate && endDate) {
+        return wordDate >= startDate && wordDate <= endDate;
+      } else if (startDate) {
+        return wordDate >= startDate;
+      } else if (endDate) {
+        return wordDate <= endDate;
+      } else {
+        return true;
+      }
+    });
+
   //sortメソッドで並び替え
   if (sortOrder === "日付順（新しい順）") {
-    filteredWords.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    filteredWords.sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
   } else if (sortOrder === "日付順（古い順）") {
-    filteredWords.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    filteredWords.sort(
+      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+    );
   } else if (sortOrder === "優先度順（高い順）") {
     filteredWords.sort((a, b) => b.priority - a.priority);
   } else if (sortOrder === "優先度順（低い順）") {
@@ -50,7 +63,9 @@ const VocabList: React.FC<VocabListProps> = ({ settings }) => {
   }
 
   // 指定された個数だけ表示
-  filteredWords = filteredWords.slice(0, displayCount);
+  const startIndex = (currentPage - 1) * displayCount;
+  const endIndex = startIndex + displayCount;
+  const paginatedWords = filteredWords.slice(startIndex, endIndex);
 
   return (
     <>
@@ -67,7 +82,7 @@ const VocabList: React.FC<VocabListProps> = ({ settings }) => {
             </div>
           </div>
           <div className="space-y-3">
-            {filteredWords.map((word, index) => (
+            {paginatedWords.map((word, index) => (
               <div key={index} className="flex items-center ">
                 <div className="flex justify-center items-center h-10 w-10 bg-gray-300 rounded-full text-lg font-bold mr-4">
                   {word.priority}
@@ -93,6 +108,25 @@ const VocabList: React.FC<VocabListProps> = ({ settings }) => {
             ))}
           </div>
         </div>
+      </div>
+      <div className="flex items-center justify-center mt-4 space-x-5">
+        <button
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md disabled:opacity-50"
+        >
+          前へ
+        </button>
+        <span className="text-gray-700">
+          ページ {currentPage} / {Math.ceil(filteredWords.length / displayCount)}
+        </span>
+        <button
+          onClick={handleNextPage}
+          disabled={endIndex >= filteredWords.length}
+          className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md disabled:opacity-50"
+        >
+          次へ
+        </button>
       </div>
     </>
   );
