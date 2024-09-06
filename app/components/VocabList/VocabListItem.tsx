ff8fddfac5146c2956aa5b1751edcae86c1f068f
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 05:51:40 +0900

    These are old data for backup

--- app/components/VocabList/VocabListItem.tsx
+++ app/components/VocabList/VocabListItem.tsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { Word } from "@/types/word";
+// import React from "react";
+// import { WordType } from "@/types/Types";
 
-interface VocabListItemProps {
-  word: Word;
-}
+// interface VocabListItemProps {
+//   word: <WordType
+// }
 
-const VocabListItem : React.FC<VocabListItemProps> = ({ word }) => {
-  return (
-    <div className="flex items-center ">
-      <div className="flex justify-center items-center h-10 w-10 bg-gray-300 rounded-full text-lg font-bold mr-4">
-        {word.priority}
-      </div>
-      <div className="flex-1 grid grid-cols-8 border-gray-200 bg-white border shadow rounded-lg px-4 py-3">
-        <div className="col-span-1 flex items-center  border-r border-gray-200 pr-3 font-bold">
-          {word.word}
-        </div>
-        <div className="col-span-1 flex items-center justify-center border-r border-gray-200 px-3">
-          {word.meaning}
-        </div>
-        <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
-          {word.example}
-        </div>
-        <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
-          {word.exampleTranslation}
-        </div>
-        <div className="col-span-2 flex items-center pl-3 text-sm">
-          {word.note}
-        </div>
-      </div>
-    </div>
-  );
-};
+// const VocabListItem : React.FC<VocabListItemProps> = ({ word }) => {
+//   return (
+//     <div className="flex items-center ">
+//       <div className="flex justify-center items-center h-10 w-10 bg-gray-300 rounded-full text-lg font-bold mr-4">
+//         {word.priority}
+//       </div>
+//       <div className="flex-1 grid grid-cols-8 border-gray-200 bg-white border shadow rounded-lg px-4 py-3">
+//         <div className="col-span-1 flex items-center  border-r border-gray-200 pr-3 font-bold">
+//           {word.word}
+//         </div>
+//         <div className="col-span-1 flex items-center justify-center border-r border-gray-200 px-3">
+//           {word.meaning}
+//         </div>
+//         <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
+//           {word.example}
+//         </div>
+//         <div className="col-span-2 flex items-center justify-center border-r border-gray-200 px-3 text-sm">
+//           {word.exampleTranslation}
+//         </div>
+//         <div className="col-span-2 flex items-center pl-3 text-sm">
+//           {word.note}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default VocabListItem;
+// export default VocabListItem;
