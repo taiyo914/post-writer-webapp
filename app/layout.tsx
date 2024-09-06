ba4c8c3222268b6737a52fa58797009be6f6481d
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 05:45:20 +0900

    Delete Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +17,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>  
-        <Header />
-        <main className="min-h-screen">{children}</main>
+        {children}
       </body>
     </html>
   );
