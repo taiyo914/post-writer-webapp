653ef785bf050c945ed50e3b24cee2bccdb5de3e
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 05:53:50 +0900

    Make Pagination Component

---
+++ app/components/Display/components/Pagination.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { ChevronLeftIcon, ChevronRightIcon, DocumentIcon } from "@heroicons/react/24/outline";
+import { div } from "framer-motion/client";
+
+interface PaginationProps {
+  pageOffset: number;
+  displayCount: number;
+  totalWords: number;
+  incrementPageOffset: () => void;
+  decrementPageOffset: () => void;
+}
+
+const Pagination = ({
+  pageOffset,
+  displayCount,
+  totalWords,
+  incrementPageOffset,
+  decrementPageOffset,
+}: PaginationProps) => {
+  return (
+   <div>
+      <div className="flex justify-end items-center my-2 mx-2 ">
+        <button
+          onClick={decrementPageOffset}
+          disabled={pageOffset === 1}
+          className={`p-1 rounded-full transition duration-200 ${
+            pageOffset === 1 ? "opacity-30" : "hover:bg-gray-100"
+          }`}
+        >
+          <ChevronLeftIcon className="h-6 text-gray-600" />
+        </button>
+        <span className="bg-gray-100 py-1 px-3 mx-1 rounded-md text-lg font-medium text-gray-700">
+          {pageOffset}
+        </span>
+        <span className="text-sm text-gray-500 mr-1"> / {
+          totalWords !== 0
+          ? (Math.ceil(totalWords / displayCount))
+          : "..."
+        }</span>
+        <button
+          onClick={incrementPageOffset}
+          disabled={ pageOffset * displayCount >= totalWords}
+          className={`p-1 rounded-full transition duration-200 ${
+            totalWords !== 0 && pageOffset * displayCount >= totalWords
+            ? "opacity-30"
+            : "hover:bg-gray-100"
+            // "totalWords !== 0 &&"は初回レンダリングで2つ目の四季が計算できないときにfalseを返し、ボタンが一瞬透明になるのを防ぐため
+          }`}
+        >
+          <ChevronRightIcon className="h-6 text-gray-600" />
+        </button>
+    </div>
+  </div>
+  );
+};
+
+export default Pagination;
