b4e50ae392601f0d31ac96ecd022d45d5bfe0bf4
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 21:37:38 +0900

    Minor test adjustment

--- app/components/Display/header/SearchInput.tsx
+++ app/components/Display/header/SearchInput.tsx
@@ -7,30 +7,8 @@ import { motion } from "framer-motion";
 import { useState, useEffect, useRef } from "react";
 
 const SearchInput = () => {
-  const [screenSize, setScreenSize] = useState("mobile");
-  const [inputValue, setInputValue] = useState("");
-
-  // ウィンドウ幅に応じてHTML自体をかえる記述
-  //ただしパフォーマンスに影響があるかも...
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-
-      if (width < 640) {
-        setScreenSize("mobile");
-      } else if (width >= 640) {
-        setScreenSize("tablet");
-      }
-      //好きなサイズと名前を追加できます
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  //あとは{screenSize === "mobile" && (<JSX/>)}という記述で設定できます！
-
-  //Former Motionの記述
   const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
   const inputRef = useRef<HTMLInputElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
 
@@ -41,7 +19,7 @@ const SearchInput = () => {
     if (!isOpen) {
       setTimeout(() => {
         inputRef.current?.focus();
-      }, 300);
+      }, 200);
     }
   };
 
@@ -69,14 +47,15 @@ const SearchInput = () => {
   }, [inputValue]);
 
   return (
-    <div ref={containerRef} className="flex items-center">
-      <button onClick={toggleSearch} className="ml-2">
-        <MagnifyingGlassIcon className="h-5 text-gray-400 cursor-pointer mx-1" />
+    <div ref={containerRef} className="flex items-center ">
+      <button onClick={toggleSearch} className="flex items-center">
+        <MagnifyingGlassIcon className="h-5 text-gray-400 cursor-pointer mr-1" />
+        {!isOpen && <span className="text-gray-500 transition-all">検索</span>}   {/* 開いたときは「検索」を非表示 */}
       </button>
       <motion.div
         initial={{ width: 0, opacity: 0 }}
         animate={{ width: isOpen ? 150 : 0, opacity: isOpen ? 1 : 0 }}
-        transition={{ duration: 0.3, ease: "easeOut" }}
+        transition={{ duration: 0.2, ease: "easeOut" }}
         className="overflow-hidden"
       >
         <div className="relative">
@@ -85,8 +64,8 @@ const SearchInput = () => {
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-            className="py-1 pl-1 border-b focus:outline-none"
-            placeholder="検索"
+            className="py-1 pl-1 focus:outline-none"
+            placeholder="入力して検索..."
             style={{ width: "100%" }}
           />
           {/* 入力があればバツボタンを表示 */}
