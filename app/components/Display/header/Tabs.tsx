f7ef507ae081fd6ffe39b545226a01f7d31dde3d
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 20:25:45 +0900

    Implement Tabs with Zustand

---
+++ app/components/Display/header/Tabs.tsx
@@ -0,0 +1,38 @@
+import useTabStore from "../store/curretTabStore";
+
+const Tabs = () => {
+  const { currentTab, setTab } = useTabStore();
+
+  return (
+    <div className="flex">
+      <div
+        onClick={() => setTab('cards')}
+        className={`
+          cursor-pointer 
+          py-1 px-5
+          border-t border-l 
+          rounded-tl-md rounded-tr-md 
+          ${currentTab === 'cards' 
+            ? 'bg-gray-200 font-semibold' 
+            : 'hover:bg-gray-100 transition-all'}`}
+      >
+        カード
+      </div>
+      <div
+        onClick={() => setTab('table')}
+        className={`
+          cursor-pointer 
+          py-1 px-3 
+          border-t border-r 
+          rounded-tl-md rounded-tr-md 
+          ${currentTab === 'table' 
+            ? 'bg-gray-200 font-semibold' 
+            : 'hover:bg-gray-100  transition-all'}`}
+      >
+        テーブル
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
