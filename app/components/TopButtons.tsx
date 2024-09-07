ef56479c4a70319f6b5b6ecb427c133b99d341c3
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-08 00:33:40 +0900

    Modified to be be sesponsive

--- app/components/TopButtons.tsx
+++ app/components/TopButtons.tsx
@@ -5,17 +5,27 @@ import Link from "next/link";
 
 const TopButtons = () => {
   return (
-    <div className="p-5 mx-auto max-w-[1200px]">
-      <div className="flex justify-center space-x-4 xl:space-x-10">
+    <div className="p-3 xs:p-5 mx-auto max-w-[1200px]">
+      <div className="flex jusity-center flex-col xs:flex-row xs:space-x-4 xl:space-x-10">
         <Link
           href="new"
-          className="text-center w-1/2 py-2  font-semibold rounded-md shadow-md hover:bg-gray-200  transition-all duration-300 border"
+          className="
+            text-center font-semibold
+            w-full xs:w-1/2 py-2   
+            border rounded-md shadow-md 
+            hover:bg-gray-200  transition-all duration-300 "
         >
           単語を追加する
         </Link>
         <Link
           href="review"
-          className="text-center w-1/2 py-2 border font-semibold bg-gray-300 rounded-md shadow-md hover:bg-gray-400 transition-all duration-300"
+          className="
+            text-center font-semibold 
+            w-full xs:w-1/2 py-2 
+            mt-2 xs:mt-0
+            bg-gray-300 
+            rounded-md shadow-md 
+            hover:bg-gray-400 transition-all duration-300"
         >
           TSV / CSV からインポート
         </Link>
