f6ed6d6ce420aceb41e0a06e19dff6a90130b67d
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 17:51:38 +0900

    Organize files

--- app/components/Display/Display.tsx
+++ app/components/Display/Display.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useState, useEffect } from "react";
 import SettingsModal from "./SettingsModal";
-import DisplayHeader from "./components/DisplayHeader";
-import CardsDisplay from "./components/CardsDisplay";
-import TableDisplay from "./components/TableDisplay";
+import DisplayHeader from "./header/DisplayHeader";
+import CardsDisplay from "./content/CardsDisplay";
+import TableDisplay from "./content/TableDisplay";
 import { InitialInfoProps } from "@/types/Types";
 
 export default function Display({
