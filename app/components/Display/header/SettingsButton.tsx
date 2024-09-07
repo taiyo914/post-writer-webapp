6ce32abb787d752fb946df981551ae2f3eb4a4dc
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-07 20:25:22 +0900

    Implement Settings Button

--- app/components/Display/header/SettingsButton.tsx
+++ app/components/Display/header/SettingsButton.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import useSettingsModalStore from "../store/settingsModalStore";
 
-export default function SettingsButton() {
+const SettingsButton = () => {
+  const { toggleModal } = useSettingsModalStore();
+
   return (
-    <div>SettingsButton</div>
-  )
-}
+    <button
+      onClick={toggleModal}
+      className="cursor-pointer flex items-center text-gray-500"
+    >
+      <AdjustmentsHorizontalIcon className="h-6 w-6 text-gray-400" />
+      フィルター
+    </button>
+  );
+};
+
+export default SettingsButton;
+
