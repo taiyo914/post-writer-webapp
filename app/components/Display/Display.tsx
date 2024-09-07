85e53c96c66fb44a0e4f9caee040655343fbc769
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-08 00:35:25 +0900

    Cleaned up by splitting the component

--- app/components/Display/Display.tsx
+++ app/components/Display/Display.tsx
@@ -5,26 +5,15 @@ import DisplayHeader from "./header/DisplayHeader";
 import CardsDisplay from "./content/CardsDisplay";
 import TableDisplay from "./content/TableDisplay";
 import { InitialInfoProps } from "@/types/Types";
+import DisplayContent from "./content/DisplayContent";
 
 export default function Display({initialWords, userId, initialUserWordsSettings,}: InitialInfoProps) {
 
   return (
-    <div className="px-5 mx-auto max-w-[2000px]">
+    <div className="px-3 xs:px-5 mx-auto max-w-[2000px]">
       <SettingsModal userId = {userId} initialUserWordsSettings={initialUserWordsSettings} />
       <DisplayHeader/>
-      {false ? (
-        <div className=" rounded rounded-tl-none border">
-          <CardsDisplay />
-        </div>
-      ) : (
-        <div className=" rounded rounded-tl-none border">
-          <TableDisplay
-            userId={userId}
-            initialUserWordsSettings={initialUserWordsSettings}
-            initialWords={initialWords}
-          />
-        </div>
-      )}
+      <DisplayContent userId={userId} initialUserWordsSettings={initialUserWordsSettings} initialWords={initialWords}/>
     </div>
   );
 }
