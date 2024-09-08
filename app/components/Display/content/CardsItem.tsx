d55effd7b349d833f5e084e83cd89fbeba87bbd8
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-08 12:33:15 +0900

    Minor change

--- app/components/Display/content/CardsItem.tsx
+++ app/components/Display/content/CardsItem.tsx
@@ -6,7 +6,7 @@ const CardsItem = ({ word }:{ word: WordType}) => {
   return (
     <motion.div
       className="shadow-md px-4 py-6 rounded-lg border border-gray-200 flex items-center justify-center"
-      whileHover={{ scale: 1.08 }}
+      whileHover={{ scale: 1.07 }}
     >
       <h2 className="text-lg font-bold">{word.word}</h2>
     </motion.div>
