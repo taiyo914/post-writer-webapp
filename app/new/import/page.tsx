dee1c00a927ff62fbf7fe9fa4013a6230268d8b0
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-09 15:06:24 +0900

    Implement Reverse option button

--- app/new/import/page.tsx
+++ app/new/import/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import { ArrowUturnLeftIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 import { csvParseRows, tsvParseRows } from "d3-dsv";
 
 const DataForm: React.FC = () => {
-  const supabase = createClient()
-  const [userId, setUserId] = useState("")
+  const supabase = createClient();
+  const [userId, setUserId] = useState("");
   const [isTSV, setIsTSV] = useState<boolean>(true);
   const [data, setData] = useState<string>("");
+  const [isReverse, setIsReverse] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -40,7 +41,7 @@ const DataForm: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     if (!userId) {
       const {
         data: { user },
@@ -48,8 +49,8 @@ const DataForm: React.FC = () => {
       } = await supabase.auth.getUser();
       if (userError || !user) {
         alert(`ユーザー情報の取得に失敗しました: ${userError?.message}`);
-      } else{
-        setUserId(user.id)
+      } else {
+        setUserId(user.id);
       }
     }
 
@@ -72,10 +73,10 @@ const DataForm: React.FC = () => {
           memo: row[4] || "",
           index: row[5] ? parseInt(row[5], 10) : 0,
         }));
-    console.log(parsedData)
-    const { error } = await supabase.from("words").insert(parsedData);
+    const finalData = isReverse ? parsedData.reverse() : parsedData;
+    const { error } = await supabase.from("words").insert(finalData);
     if (error) {
-      alert(`単語の保存に失敗しました: ${error.message}`)
+      alert(`単語の保存に失敗しました: ${error.message}`);
     } else {
       router.push("/");
     }
@@ -84,23 +85,22 @@ const DataForm: React.FC = () => {
   return (
     <div className="py-4 xs:px-5 px-3 mx-auto max-w-3xl">
       <div className="px-1 my-2">
-      <Link
-            href="/new"
-            className="
+        <Link
+          href="/new"
+          className="
               text-gray-500 rounded-2xl
               p-1 px-2 w-fit
               hover:text-gray-700 hover:bg-gray-200 transition duration-200 
               flex items-center space-x-1"
-          >
-            <ArrowUturnLeftIcon className="h-4" />
-            <div>戻る</div>
-          </Link>
+        >
+          <ArrowUturnLeftIcon className="h-4" />
+          <div>戻る</div>
+        </Link>
       </div>
       <form
         onSubmit={handleSubmit}
         className="flex flex-col items-center p-6 bg-white text-black rounded-md shadow-lg border"
       >
-        
         <div className="flex items-center mb-3 space-x-2">
           <button
             type="button"
@@ -121,15 +121,13 @@ const DataForm: React.FC = () => {
             CSV
           </button>
         </div>
-        <h3 className="text-lg font-bold mb-2">
-          {isTSV ? "TSV" : "CSV"}形式でテキストを入力
-        </h3>
-        <p className="mb-4 text-gray-500 text-sm"> 
+        <h3 className="text-lg font-bold mb-2">{isTSV ? "TSV" : "CSV"}形式でテキストを入力</h3>
+        <p className="mb-4 text-gray-500 text-sm">
           {isTSV ? (
             <>
-             TSVはデータを<strong>タブ</strong>で区切って記述するファイル形式です
+              TSVはデータを<strong>タブ</strong>で区切って記述するファイル形式です
             </>
-          ) :(
+          ) : (
             <>
               CSVはデータを<strong>カンマ</strong>で区切って記述するファイル形式です
             </>
@@ -146,11 +144,25 @@ const DataForm: React.FC = () => {
           onChange={handleChange}
           required
         />
+
+        <div className="mt-4 flex items-center space-x-2">
+          <div className="text-sm text-gray-600">下から順番に追加する</div>
+          <div 
+            className="h-5 w-5" 
+            onClick={() => setIsReverse(!isReverse)}
+          >
+            {isReverse 
+            ? <CheckIcon className="h-5 w-5 border-blue-500 border rounded bg-blue-100"/>
+            : <div className="h-5 w-5 rounded border  border-gray-600"></div>
+          }
+          </div>
+        </div>
+
         <button
           type="submit"
           className="w-2/3 mt-4 py-3 bg-black text-white font-semibold rounded-full hover:opacity-75 focus:outline-none transition"
         >
-          {isTSV?"TSV":"CSV"}からインポート
+          {isTSV ? "TSV" : "CSV"}からインポート
         </button>
       </form>
     </div>
