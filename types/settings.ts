5b3202f5117e71fc98de580da29a98b34e7e71fc
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-09 13:54:34 +0900

    Removed unnecessary files

--- types/settings.ts
+++
@@ -1,7 +0,0 @@
-export type SettingsProps = {
-  sortOrder: string;
-  // "日付順（新しい順）" | "日付順（古い順）" | "優先度順（高い順 " | "優先度順（低い順）"としたほうがより厳密
-  displayCount: number;
-  priorityRange: [number, number];
-  dateRange: [string, string];
-};
